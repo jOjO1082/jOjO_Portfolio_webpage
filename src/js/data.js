bf1d78a21a8fa0561d013da9c4988294e4880f28
Jonathan Joseph <klassiqq1083@gmail.com>
2026-01-15 18:57:09 +0100

    Proper Tailwind update

---
+++ src/js/data.js
@@ -0,0 +1,212 @@
+export const myServices = [
+
+    {
+        id : 1,
+        title : "Web Development",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+
+    {
+        id : 2,
+        title : "UI/UX Design",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+
+    {
+        id : 3,
+        title : "Digital Marketing",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+
+    {
+        id : 4,
+        title : "graphics Design",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+
+    {
+        id : 5,
+        title : "Video Editing",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."    
+    },
+
+    {
+        id : 6,
+        title : "Content Writing",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+
+    {
+        id : 7,
+        title : "Branding",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+
+    {
+        id : 8,
+        title : "Photography",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+
+    {
+        id : 9,
+        title : "Design Systems",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    }
+
+]
+
+
+export const myTools = [
+    {
+        id : 1,
+        title : "HTML",
+        img : "images/tools/html.png",
+        skillLevel : "92%"
+    },
+
+    {
+        id : 2,
+        title : "CSS",
+        img : "images/tools/css.png",
+        skillLevel : "80%"
+    },
+
+    {
+        id : 3,
+        title : "Javascript",
+        img : "images/tools/js.png",
+        skillLevel : "65%"
+    },
+
+    {
+        id : 4,
+        title : "React",
+        img : "images/tools/react.png",
+        skillLevel : "10%"
+    },  
+
+    {
+        id : 5,
+        title : "Tailwind",
+        img : "images/tools/tailwind.png",
+        skillLevel : "98%"
+    },
+
+    {
+        id : 6,
+        title : "Figma",
+        img : "images/tools/figma.png",
+        skillLevel : "95%"
+    },
+
+    {
+        id : 7,
+        title : "Photoshop",
+        img : "images/tools/photoshop.png",
+        skillLevel : "70%"
+    },
+
+    {
+        id : 8,
+        title : "Illustrator",
+        img : "images/tools/illustrator.png",
+        skillLevel : "70%"
+    },
+
+    {
+        id : 9,
+        title : "WordPress",
+        img : "images/tools/wp.png",
+        skillLevel : "90%"
+    },
+
+    {
+        id : 10,
+        title : "Indesign",
+        img : "images/tools/indesign.png",
+        skillLevel : "90%"
+    }
+]
+
+
+export const projects = [
+    {
+        id : 1,
+        title : "Project 1",
+        category : "Web Development",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+    
+    {
+        id : 2,
+        title : "Project 2",
+        category : "UI/UX Design",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+    
+    {
+        id : 3,
+        title : "Project 3",
+        category : "Graphic Design",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    }
+]
+
+
+export const testimonials = [
+    {
+        id : 1,
+        name : "John Doe",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        role: "Web Designer",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+    
+    {
+        id : 2,
+        name : "John Doe",
+        img : "https://images.unsplash.com/photo-1519389950473-47ba561d188a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+        role: "Web Designer",
+        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    }
+]
+
+
+export const faqData = [
+    {
+        id : 1,
+        question : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat.",
+        answer : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+    
+    {
+        id : 2,
+        question : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat.",
+        answer : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+    
+    {
+        id : 3,
+        question : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat.",
+        answer : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    },
+
+    {
+        id : 4,
+        question : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat.",
+        answer : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+    }
+]
---
+++ src/js/index.js
@@ -0,0 +1,143 @@
+import { myTools } from "./data.js";
+import { faqData } from "./data.js";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // Render all tools to the frontend
+    function renderTools(){
+        const myToolsContainer = document.querySelector('.tools-cards-cont')
+
+        if (!myToolsContainer) return
+
+        myToolsContainer.innerHTML = '';
+
+        myTools.forEach(eachTool => {
+
+
+        myToolsContainer.innerHTML += `
+        
+        <div class="tool-card p-4 bg-slate-50 w-full h-auto flex flex-col justify-start items-end gap-4 rounded-[24px] border-l-4 border-primary-dark hover:bg-white hover:shadow-sm transition ease-out delay-75 hover:transition-all">
+              <div class="percentage bg-primary-dark p-4 rounded-full w-fit">
+                <p class="font-bold text-white text-[18px]">${eachTool.skillLevel}</p>
+              </div>
+
+              <div class="tool-icon-and-name w-full h-auto flex flex-row justify-start items-center gap-2">
+                <div class="w-12 h-12 tool-icon-cont bg-white p-4 rounded-full">
+                  <img
+                    src="${eachTool.img}"
+                    alt="" class="w-full h-full object-cover bg-cover"
+                  />
+                </div>
+
+                <h3 class="text-3xl font-medium">${eachTool.title}</h3>
+              </div>
+            </div>
+        
+        `
+    })
+    }
+
+    renderTools()
+
+
+    
+
+    // Menu reveal and close
+
+    const menuRevealButton = document.getElementById('menu-button');
+    const menuContainer = document.getElementById('mobile-menu')
+    const closeMobileMenuButton = document.getElementById('close-icon')
+
+
+    menuRevealButton.addEventListener('click', () => {
+        menuContainer.classList.remove('-translate-y-[100%]')
+        menuContainer.classList.add('translate-y-0')
+    })
+
+    closeMobileMenuButton.addEventListener('click', () => {
+        menuContainer.classList.add('-translate-y-[100%]');
+        menuContainer.classList.remove('translate-y-0')
+    })
+
+
+
+
+
+
+    // Render FAQs to the frontend
+
+    function renderFaqs(){
+        const faqContainer = document.querySelector('.faq-dropdown-block-container')
+
+        if (!faqContainer) return
+
+        faqContainer.innerHTML = '';
+
+        faqData.forEach(eachFaq => {
+            faqContainer.innerHTML += `
+            
+            <div class="faq-blocks w-full h-auto flex flex-col justify-center items-center gap-2  bg-slate-50 p-4 rounded-[24px] border-l-4 border-primary-dark hover:bg-white hover:shadow-sm transition ease-out delay-75 hover:transition-all cursor-pointer">
+                <div class="question w-full h-auto flex flex-row justify-between items-center">
+                  <h4 class="font-semibold text-[16px]">${eachFaq.question}</h4>
+                  <i class="fa-solid fa-angle-down openFaq"></i>
+                </div>
+  
+                <div class=" w-full h-auto answer hidden">
+                  <p class="text-[16px]">${eachFaq.answer}
+                  </p>
+                </div>
+              </div>
+            
+            `
+        })
+
+    }
+
+    renderFaqs()
+
+
+
+    // Open and close FAQs
+
+    const allFaqs = document.querySelectorAll('.faq-blocks')
+    // const allAnswers = document.querySelectorAll('.faq-blocks .answer')
+
+
+    allFaqs.forEach(eachClicked => {
+        eachClicked.addEventListener('click', () => {
+            const answer = eachClicked.querySelector('.answer')
+
+            answer.classList.toggle('hidden')
+                      
+        })
+    })
+    
+
+    
+
+    
+
+    
+    // Active link state
+    const allNavLinks = document.querySelectorAll('#link a')
+
+    console.log(allNavLinks);
+    
+
+    allNavLinks.forEach(eachLink => {
+        eachLink.addEventListener('click', () => {
+            allNavLinks.forEach(link => {
+                link.classList.remove('text-tertiary', 'font-bold' )
+                link.classList.add('text-primary-dark')
+            })
+            eachLink.classList.remove('text-primary-dark')
+            eachLink.classList.add('text-tertiary', 'font-bold')
+        })
+    })
+
+    
+
+    
+
+});
