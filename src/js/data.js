e35bb0b055a392f5586bbac039bc9b0de1de7343
Jonathan Joseph <klassiqq1083@gmail.com>
2026-01-19 13:12:00 +0100

    Links

--- src/js/data.js
+++ src/js/data.js
@@ -4,58 +4,65 @@ export const myServices = [
         id : 1,
         title : "Web Development",
         img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-adjustments-code"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 10a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M6 4v4" /><path d="M6 12v8" /><path d="M13.557 14.745a2 2 0 1 0 -1.557 3.255" /><path d="M12 4v10" /><path d="M12 18v2" /><path d="M16 7a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M18 4v1" /><path d="M18 9v4" /><path d="M20 21l2 -2l-2 -2" /><path d="M17 17l-2 2l2 2" /></svg>',
-        text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat.",
-        servicesDetails: {
-            
-        }
+        text : "Fast, responsive, and user‑friendly websites built with modern tools.",
     },
 
     {
         id : 2,
         title : "UI/UX Design",
-        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-transform-point-top-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" fill="currentColor" /><path d="M3 18a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M17 4a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M17 18a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M11 5h2" /><path d="M5 11v2" /><path d="M19 11v2" /><path d="M11 19h2" /></svg>'
+        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-transform-point-top-left"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" fill="currentColor" /><path d="M3 18a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M17 4a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M17 18a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M11 5h2" /><path d="M5 11v2" /><path d="M19 11v2" /><path d="M11 19h2" /></svg>',
+        text : " Intuitive interfaces rooted in research, usability, and human‑centered design.",
+        text: "Intuitive interfaces rooted in research, usability, and human‑centered design."
+
     },
 
     {
         id : 3,
         title : "Digital Marketing",
-        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-ad-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M2 12a10 10 0 1 0 20 0a10 10 0 1 0 -20 0" /><path d="M7 15v-4.5a1.5 1.5 0 0 1 3 0v4.5" /><path d="M7 13h3" /><path d="M14 9v6h1a2 2 0 0 0 2 -2v-2a2 2 0 0 0 -2 -2h-1" /></svg>'
+        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-ad-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M2 12a10 10 0 1 0 20 0a10 10 0 1 0 -20 0" /><path d="M7 15v-4.5a1.5 1.5 0 0 1 3 0v4.5" /><path d="M7 13h3" /><path d="M14 9v6h1a2 2 0 0 0 2 -2v-2a2 2 0 0 0 -2 -2h-1" /></svg>',
+        text : "Strategic campaigns, content design, and digital visibility optimization.",
     },
 
     {
         id : 4,
         title : "Graphics Design",
-        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-adobe-photoshop"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12c0 -4.243 0 -6.364 1.318 -7.682s3.44 -1.318 7.682 -1.318s6.364 0 7.682 1.318s1.318 3.44 1.318 7.682s0 6.364 -1.318 7.682s-3.44 1.318 -7.682 1.318s-6.364 0 -7.682 -1.318s-1.318 -3.44 -1.318 -7.682" /><path d="M6.79 15.79v-3.79m0 0v-2.724c0 -.11 0 -.165 .004 -.211c.044 -.45 .4 -.806 .85 -.85c.046 -.004 .101 -.004 .211 -.004h1.303a1.895 1.895 0 1 1 0 3.789l-2.368 0" /><path d="M17.178 11.263c-.164 -.659 -.935 -1.158 -1.862 -1.158c-1.047 0 -1.895 .637 -1.895 1.421c0 .785 .848 1.421 1.895 1.421c1.046 0 1.895 .637 1.895 1.421c0 .785 -.849 1.421 -1.895 1.421c-.93 0 -1.704 -.502 -1.864 -1.165" /></svg>'
+        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-adobe-photoshop"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12c0 -4.243 0 -6.364 1.318 -7.682s3.44 -1.318 7.682 -1.318s6.364 0 7.682 1.318s1.318 3.44 1.318 7.682s0 6.364 -1.318 7.682s-3.44 1.318 -7.682 1.318s-6.364 0 -7.682 -1.318s-1.318 -3.44 -1.318 -7.682" /><path d="M6.79 15.79v-3.79m0 0v-2.724c0 -.11 0 -.165 .004 -.211c.044 -.45 .4 -.806 .85 -.85c.046 -.004 .101 -.004 .211 -.004h1.303a1.895 1.895 0 1 1 0 3.789l-2.368 0" /><path d="M17.178 11.263c-.164 -.659 -.935 -1.158 -1.862 -1.158c-1.047 0 -1.895 .637 -1.895 1.421c0 .785 .848 1.421 1.895 1.421c1.046 0 1.895 .637 1.895 1.421c0 .785 -.849 1.421 -1.895 1.421c-.93 0 -1.704 -.502 -1.864 -1.165" /></svg>',
+        text : "High‑quality designs for digital and print communication."
     },
 
     {
         id : 5,
         title : "Video Editing",
-        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-adobe-premiere"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12c0 -4.243 0 -6.364 1.318 -7.682s3.44 -1.318 7.682 -1.318s6.364 0 7.682 1.318s1.318 3.44 1.318 7.682s0 6.364 -1.318 7.682s-3.44 1.318 -7.682 1.318s-6.364 0 -7.682 -1.318s-1.318 -3.44 -1.318 -7.682" /><path d="M7.263 15.79v-3.79m0 0v-3.248c0 -.335 .222 -.541 .542 -.541h1.353a1.895 1.895 0 1 1 0 3.789l-1.895 0" /><path d="M13.895 10.579v1.895m0 0v3.315m0 -3.315c.531 -.709 1.026 -1.592 1.894 -1.832q .22 -.062 .474 -.063" /></svg>'    
+        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-adobe-premiere"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12c0 -4.243 0 -6.364 1.318 -7.682s3.44 -1.318 7.682 -1.318s6.364 0 7.682 1.318s1.318 3.44 1.318 7.682s0 6.364 -1.318 7.682s-3.44 1.318 -7.682 1.318s-6.364 0 -7.682 -1.318s-1.318 -3.44 -1.318 -7.682" /><path d="M7.263 15.79v-3.79m0 0v-3.248c0 -.335 .222 -.541 .542 -.541h1.353a1.895 1.895 0 1 1 0 3.789l-1.895 0" /><path d="M13.895 10.579v1.895m0 0v3.315m0 -3.315c.531 -.709 1.026 -1.592 1.894 -1.832q .22 -.062 .474 -.063" /></svg>',
+        text : "Clean, engaging video assets for social media and marketing purposes."    
     },
 
     {
         id : 6,
         title : "Content Writing",
-        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-letter-case"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 15.5a3.5 3.5 0 1 0 7 0a3.5 3.5 0 1 0 -7 0" /><path d="M3 19v-10.5a3.5 3.5 0 0 1 7 0v10.5" /><path d="M3 13h7" /><path d="M21 12v7" /></svg>'
+        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-letter-case"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 15.5a3.5 3.5 0 1 0 7 0a3.5 3.5 0 1 0 -7 0" /><path d="M3 19v-10.5a3.5 3.5 0 0 1 7 0v10.5" /><path d="M3 13h7" /><path d="M21 12v7" /></svg>',
+        text : "Clear, compelling copy that informs, sells, and inspires."
     },
 
     {
         id : 7,
         title : "Branding",
-        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-elastic"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 2a5 5 0 0 1 5 5c0 .712 -.232 1.387 -.5 2c1.894 .042 3.5 1.595 3.5 3.5c0 1.869 -1.656 3.4 -3.5 3.5c.333 .625 .5 1.125 .5 1.5a2.5 2.5 0 0 1 -2.5 2.5c-.787 0 -1.542 -.432 -2 -1c-.786 1.73 -2.476 3 -4.5 3a5 5 0 0 1 -4.583 -7a3.5 3.5 0 0 1 -.11 -6.992l.195 0a2.5 2.5 0 0 1 2 -4c.787 0 1.542 .432 2 1c.786 -1.73 2.476 -3 4.5 -3l-.002 -.008" /><path d="M8.5 9l-3 -1" /><path d="M9.5 5l-1 4l1 2l5 2l4 -4" /><path d="M18.499 16l-3 -.5l-1 -2.5" /><path d="M14.5 19l1 -3.5" /><path d="M5.417 15l4.083 -4" /></svg>'
+        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-elastic"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 2a5 5 0 0 1 5 5c0 .712 -.232 1.387 -.5 2c1.894 .042 3.5 1.595 3.5 3.5c0 1.869 -1.656 3.4 -3.5 3.5c.333 .625 .5 1.125 .5 1.5a2.5 2.5 0 0 1 -2.5 2.5c-.787 0 -1.542 -.432 -2 -1c-.786 1.73 -2.476 3 -4.5 3a5 5 0 0 1 -4.583 -7a3.5 3.5 0 0 1 -.11 -6.992l.195 0a2.5 2.5 0 0 1 2 -4c.787 0 1.542 .432 2 1c.786 -1.73 2.476 -3 4.5 -3l-.002 -.008" /><path d="M8.5 9l-3 -1" /><path d="M9.5 5l-1 4l1 2l5 2l4 -4" /><path d="M18.499 16l-3 -.5l-1 -2.5" /><path d="M14.5 19l1 -3.5" /><path d="M5.417 15l4.083 -4" /></svg>',
+        text : "Visual systems that communicate your story and build trust."
     },
 
     {
         id : 8,
         title : "Photography",
-        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-google-photos"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7.5 7c2.485 0 4.5 1.974 4.5 4.409v.591h-8.397a.61 .61 0 0 1 -.426 -.173a.585 .585 0 0 1 -.177 -.418c0 -2.435 2.015 -4.409 4.5 -4.409" /><path d="M16.5 17c-2.485 0 -4.5 -1.974 -4.5 -4.409v-.591h8.397c.333 0 .603 .265 .603 .591c0 2.435 -2.015 4.409 -4.5 4.409" /><path d="M7 16.5c0 -2.485 1.972 -4.5 4.405 -4.5h.595v8.392a.61 .61 0 0 1 -.173 .431a.584 .584 0 0 1 -.422 .177c-2.433 0 -4.405 -2.015 -4.405 -4.5" /><path d="M17 7.5c0 2.485 -1.972 4.5 -4.405 4.5h-.595v-8.397a.61 .61 0 0 1 .175 -.428a.584 .584 0 0 1 .42 -.175c2.433 0 4.405 2.015 4.405 4.5" /></svg>'
+        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-brand-google-photos"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7.5 7c2.485 0 4.5 1.974 4.5 4.409v.591h-8.397a.61 .61 0 0 1 -.426 -.173a.585 .585 0 0 1 -.177 -.418c0 -2.435 2.015 -4.409 4.5 -4.409" /><path d="M16.5 17c-2.485 0 -4.5 -1.974 -4.5 -4.409v-.591h8.397c.333 0 .603 .265 .603 .591c0 2.435 -2.015 4.409 -4.5 4.409" /><path d="M7 16.5c0 -2.485 1.972 -4.5 4.405 -4.5h.595v8.392a.61 .61 0 0 1 -.173 .431a.584 .584 0 0 1 -.422 .177c-2.433 0 -4.405 -2.015 -4.405 -4.5" /><path d="M17 7.5c0 2.485 -1.972 4.5 -4.405 4.5h-.595v-8.397a.61 .61 0 0 1 .175 -.428a.584 .584 0 0 1 .42 -.175c2.433 0 4.405 2.015 4.405 4.5" /></svg>',
+        text : "Creative visuals that elevate product storytelling."
     },
 
     {
         id : 9,
         title : "Design Systems",
-        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-vector-bezier"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 15a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M17 15a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M10 7a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M10 8.5a6 6 0 0 0 -5 5.5" /><path d="M14 8.5a6 6 0 0 1 5 5.5" /><path d="M10 8l-6 0" /><path d="M20 8l-6 0" /><path d="M2 8a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M20 8a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg>'
+        img : '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-vector-bezier"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 15a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M17 15a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M10 7a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1l0 -2" /><path d="M10 8.5a6 6 0 0 0 -5 5.5" /><path d="M14 8.5a6 6 0 0 1 5 5.5" /><path d="M10 8l-6 0" /><path d="M20 8l-6 0" /><path d="M2 8a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /><path d="M20 8a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" /></svg>',
+        text : "Scalable, reusable design components for consistent product experience."
     }
 
 ]
@@ -207,26 +214,58 @@ export const testimonials = [
 export const faqData = [
     {
         id : 1,
-        question : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat.",
-        answer : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+        question : "What services do you offer?",
+        answer : "I provide end‑to‑end digital product support including: UI/UX design, web development, and digital marketing. If your project involves design, development, or digital communication, I can help you bring it to life."
     },
     
     {
         id : 2,
-        question : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat.",
-        answer : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+        question : "What is your design process like?",
+        answer : "My process is structured, but flexible enough to fit your project needs: from concept to launch. I work with you to understand your vision and goals, and then design a solution that meets your needs. I then implement it, and keep you updated with the progress."
     },
     
     {
         id : 3,
-        question : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat.",
-        answer : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+        question : " Do you work remotely or onsite?",
+        answer : "Timelines depend on the size and complexity of the project: Simple website 1–2 weeks; UI/UX design for a landing page: 3–5 days; Multi‑page product design: 2–4 weeks; Full web design + development: 3–6 weeks; Branding package: 1–2 weeks. After discussing your project, I’ll provide a custom timeline that fits your needs."
+
+        // Simple website: 1–2 weeks UI/UX design for a landing page: 3–5 days Multi‑page product design: 2–4 weeks Full web design + development: 3–6 weeks Branding package: 1–2 weeks
     },
 
     {
         id : 4,
-        question : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat.",
-        answer : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, placeat."
+        question : "How much do your projects cost?",
+        answer : "Project pricing varies by deliverables and complexity. Typical starting ranges include: UI/UX design: $200; Web development: $500; Digital marketing: $200; Full web design + development: from $1,500; Branding package: from $1,000. For an accurate quote, simply share your goals and required features."
+    },
+
+    {
+        id : 5,
+        question : "What tools do you use?",
+        answer : "I use tools that allow me to work efficiently and deliver high‑quality results: Design-tools: Figma, Adobe Photoshop, Illustrator, InDesign; Code: HTML, CSS, JavaScript, React, TailwindCSS; Content & CMS: WordPress; Visuals: Photography, basic video editing tools"
+    },
+
+    {
+        id : 6,
+        question : "Can you redesign an existing website or product?",
+        answer : "Yes, absolutely. I specialize in revamping outdated or underperforming websites and interfaces by improving usability, layout, branding, and overall UX/UI design."
+    },
+
+    {
+        id : 7,
+        question : "Do you provide ongoing support or maintenance?",
+        answer : "Yes — I offer optional maintenance and support packages, including: monthly updates, bug fixes, and ongoing maintenance of existing projects"
+    },
+
+    {
+        id : 8,
+        question : "Can you work with my existing team or developer?",
+        answer : "Definitely. I frequently collaborate with teams, developers, project managers, and marketers. My workflow easily integrates into existing processes using shared tools and communication platforms."
+    },
+
+    {
+        id : 9,
+        question : "What do you need from me to get started?",
+        answer : "Here’s what helps jumpstart the project: a clear understanding of your vision and goals; a solid understanding of your target audience; and a clear plan for the project timeline; Required features or functionalities. If you’re not sure what you need yet, I can help you figure it out."
     }
 ]
 
